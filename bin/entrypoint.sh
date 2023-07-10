d01f55b00dff883694ac448ffb39ec5e8634fd8f
Jonathan Monette <jon.monette@gmail.com>
2023-07-10 15:00:22 -0500

    fix variable substituion

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,4 +4,4 @@ curl --fail\
   --header "Authorization: Bearer ${1}"\
   --request PUT --header "Content-Type: application/octet-stream" \
   --data-binary @${3}.csv \
-  https://api.data.world/v0/uploads/${2}/files/${$4}
+  https://api.data.world/v0/uploads/${2}/files/${4}
