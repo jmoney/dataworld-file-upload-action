1255114c97b69e7f7e36f204dee5ca4b83a001a1
Jonathan Monette <jon.monette@gmail.com>
2023-07-10 15:01:44 -0500

    fix file uploading

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,5 +3,5 @@
 curl --fail\
   --header "Authorization: Bearer ${1}"\
   --request PUT --header "Content-Type: application/octet-stream" \
-  --data-binary @${3}.csv \
+  --data-binary @${3} \
   https://api.data.world/v0/uploads/${2}/files/${4}
